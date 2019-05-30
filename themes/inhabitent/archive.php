545361aeefb0961c4994134d46feffdcf2d80a21
jonathandsilva2 <jonathandsilva2@gmail.com>
2019-05-30 05:06:06 -0400

    added archives

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -1,30 +1,27 @@
 <?php
 /**
- * The template for displaying archive pages.
+ * The template for displaying all pages.
  *
  * @package RED_Starter_Theme
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
+	<div class = journal>
+	<div id="primary" class="content-area journal-left">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+				<?php get_template_part( 'template-parts/content' ); ?>
 
 			<?php endwhile; ?>
 
@@ -40,4 +37,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
